1aa388b0ac254532cbcc3d31ec9a7d5973be4964
solomonronald <solomon.ymail@gmail.com>
2021-01-17 05:29:49 +0530

    Add precition to range distribution

--- src/main/scala/com/solomonronald/spark/fluff/distribution/RangeDist.scala
+++ src/main/scala/com/solomonronald/spark/fluff/distribution/RangeDist.scala
@@ -1,10 +1,11 @@
 package com.solomonronald.spark.fluff.distribution
 
 import org.apache.spark.sql.Column
+import org.apache.spark.sql.functions.round
 
-class RangeDist(min: Double = 0.00, max: Double = 1.00) extends FluffyDistribution {
+class RangeDist(min: Double = 0.00, max: Double = 0.00, precision: Int = 16) extends FluffyDistribution {
 
   override def getColumn(c: Column): Column = {
-    (c * ((max - min) + 1)) + min
+    round((c * ((max - min) + 1)) + min, precision)
   }
 }
