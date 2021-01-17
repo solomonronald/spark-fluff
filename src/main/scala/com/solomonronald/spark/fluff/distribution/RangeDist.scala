599391d594ec5d3bda1834f849450d40905d720e
solomonronald <solomon.ymail@gmail.com>
2021-01-17 05:35:27 +0530

    Remove eincluding upper range

--- src/main/scala/com/solomonronald/spark/fluff/distribution/RangeDist.scala
+++ src/main/scala/com/solomonronald/spark/fluff/distribution/RangeDist.scala
@@ -3,9 +3,9 @@ package com.solomonronald.spark.fluff.distribution
 import org.apache.spark.sql.Column
 import org.apache.spark.sql.functions.round
 
-class RangeDist(min: Double = 0.00, max: Double = 0.00, precision: Int = 16) extends FluffyDistribution {
+class RangeDist(min: Double = 0.00, max: Double = 1.00, precision: Int = 16) extends FluffyDistribution {
 
   override def getColumn(c: Column): Column = {
-    round((c * ((max - min) + 1)) + min, precision)
+    round((c * (max - min)) + min, precision)
   }
 }
